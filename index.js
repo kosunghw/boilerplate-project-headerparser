686e2761bf015fcb07fac15af4b7631c2f7a8dac
Sung <sung272012@hotmail.com>
2024-10-26 18:00:03 -0400

    Commit solutions

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var app = express();
 // so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(express.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -22,6 +23,17 @@ app.get('/', function (req, res) {
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+app.get('/api/whoami', (req, res) => {
+  console.log(req.headers);
+  const acceptLanguage = req.headers['accept-language'];
+  const userAgent = req.headers['user-agent'];
+  res.json({
+    ipaddress: req.ip,
+    language: acceptLanguage,
+    software: userAgent,
+  });
 });
 
 // listen for requests :)
